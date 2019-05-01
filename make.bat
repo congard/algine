aae5982dfc99cd4b3655b53f61ef91e3c4e88c85
Daniel Bernar <dbcongard@gmail.com>
2019-05-01 20:45:25 +0300

    compilation on Windows with headers

--- make.bat
+++ make.bat
@@ -4,16 +4,39 @@ rem i - install path (the place where the libraries were installed)
 rem I - include path
 rem L - library path
 
-SET i=C:/Program Files (x86)/
-SET glewI="%i%glew/include"
-SET glfwI="%i%GLFW/include"
-SET glmI="%i%glm/include"
-SET assimpI="%i%Assimp/include"
-SET glewL="%i%glew/bin"
-SET glfwL="%i%GLFW/lib"
-SET glmL="%i%glm/include/glm"
-SET assimpL="%i%Assimp/bin"
-SET l=-lassimp -lglew32 -lglfw3 -lopengl32
+rem configuration
+set i=C:/Program Files (x86)/
+set glewI="%i%glew/include"
+set glfwI="%i%GLFW/include"
+set glmI="%i%glm/include"
+set assimpI="%i%Assimp/include"
+set glewL="%i%glew/bin"
+set glfwL="%i%GLFW/lib"
+set glmL="%i%glm/include/glm"
+set assimpL="%i%Assimp/bin"
+set l=-lassimp -lglew32 -lglfw3 -lopengl32
 
-g++ -I %glewI% -I %glfwI% -I %glmI% -I %assimpI% -L %glewL% -L %glfwL% -L %glmL% -L %assimpL% src/main.cpp -o out/main.exe %l%
+set include=include
+set source=src
+set headers=math.h io.h
+set sources=main.cpp math.cpp io.cpp
+
+rem compilation
+set headers_full=
+set sources_full=
+
+setlocal EnableDelayedExpansion
+for %%v in (%headers%) do (
+    set headers_full=!headers_full! %include%/algine/%%v
+)
+
+for %%v in (%sources%) do (
+    set sources_full=!sources_full! %source%/%%v
+)
+
+g++ -I %glewI% -I %glfwI% -I %glmI% -I %assimpI%^
+    -L %glewL% -L %glfwL% -L %glmL% -L %assimpL%^
+    -I %include% %headers_full% %sources_full%^
+    -o out/main.exe %l%
+
 echo Compilation done
