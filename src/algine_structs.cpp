0e995f524b5c6f3d4b6ba33922d9315e3e28ac74
Daniel Bernar <dbcongard@gmail.com>
2019-05-15 18:28:47 +0300

    new structs, pointer replaced with vector

--- src/algine_structs.cpp
+++ src/algine_structs.cpp
@@ -2,11 +2,31 @@
 #define ALGINE_STRUCTS
 
 #include <string>
+#include <vector>
 #include <GL/glew.h>
 #include <algine/constants.h>
 #include <algine/types.h>
 
 namespace algine {
+
+struct LightIds {
+    int
+        kc,
+        kl,
+        kq,
+        pos,
+        color,
+        shadowMap;
+};
+
+struct DirLightIds : public LightIds {
+    int lightMatrix, minBias, maxBias;
+};
+
+struct PointLightIds : public LightIds {
+    int far, bias;
+};
+
 // color shader ids
 struct CShader {
     GLint
@@ -30,10 +50,10 @@ struct CShader {
 
         // fragment shader
         viewPos,
-        lampsCount,
+        pointLightsCount,
+        dirLightsCount,
         shadowDiskRadiusK,
         shadowDiskRadiusMin,
-        shadowBias,
         shadowOpacity,
         focalDepth,
         focalRange,
@@ -54,6 +74,9 @@ struct CShader {
         cinematicDOFPlaneInFocus,
         cinematicDOFAperture,
         cinematicDOFImageDistance;
+
+    std::vector<PointLightIds> pointLights;
+    std::vector<DirLightIds> dirLights;
 };
 
 // shadow shader ids
@@ -61,10 +84,14 @@ struct SShader {
     GLint
         programId,
 
+        // geometry shader
+        shadowMatrices, //  zero element id
+
         // vertex shader
         inPosition,
         inBoneIds, // zero element id
         inBoneWeights, // zero element id
+        matLightSpace,
         matModel,
         bones, // zero element id
         boneAttribsPerVertex, // bonesPerVertex / 4 + (bonesPerVertex % 4 == 0 ? 0 : 1)
@@ -89,7 +116,7 @@ struct BLUShader {
         samplerDofBuffer,
         sigmaMin,
         sigmaMax,
-        *bloomKernel;
+        bloomKernel; // zero element id
 };
 
 // blend shader ids
@@ -138,9 +165,10 @@ struct SSShader {
 };
 
 struct AlgineParams {
-    uint8
+    uint
         normalMappingMode = ALGINE_NORMAL_MAPPING_MODE_ENABLED,
-        shadowMappingMode = ALGINE_SHADOW_MAPPING_MODE_SIMPLE,
+        shadowMappingMode = ALGINE_SHADOW_MAPPING_MODE_DISABLED,
+        shadowMappingType = ALGINE_SHADOW_MAPPING_TYPE_POINT_LIGHTING,
         bloomMode = ALGINE_BLOOM_MODE_ENABLED,
         dofMode = ALGINE_DOF_MODE_ENABLED,
         textureMappingMode = ALGINE_TEXTURE_MAPPING_MODE_ENABLED,
@@ -150,7 +178,8 @@ struct AlgineParams {
         boneSystemMode = ALGINE_BONE_SYSTEM_DISABLED;
 
     uint
-        maxLampsCount = 8,
+        maxPointLightsCount = 8,
+        maxDirLightsCount = 8,
         maxBoneAttribsPerVertex = 1, // 1 bone attrib = 4 bones per vertex
         maxBones = 64, // max bones in mesh
         bloomKernelSize = 8,
