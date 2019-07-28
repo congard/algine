a9148bb9443f09b6a15d62b161b4abe3ed4d3d03
Daniel Bernar <dbcongard@gmail.com>
2019-07-28 11:54:46 +0300

    Multiplying positions by view matrix

--- src/resources/shaders/basic/cubemap_fragment.glsl
+++ src/resources/shaders/basic/cubemap_fragment.glsl
@@ -9,6 +9,8 @@ in vec3 texCoords;
 layout(location = ALGINE_CUBEMAP_COLOR_OUT_COLOR_COMPONENT) out vecout fragColor;
 layout(location = ALGINE_POS_OUT_COLOR_COMPONENT) out vec3 fragPos;
 
+uniform mat3 view; // need if you want write positions
+
 uniform samplerCube cubemap;
 uniform vecout color = vecout(1.0);
 uniform float scaling = 1.0;
@@ -19,8 +21,8 @@ void main() {
     #endif
 
     #ifdef ALGINE_CUBE_POSITIONS
-        fragPos = vecout(texCoords) * scaling;
+        fragPos = vecout(view * texCoords) * scaling;
     #elif defined ALGINE_SPHERE_POSITIONS
-        fragPos = vecout(normalize(texCoords)) * scaling;
+        fragPos = vecout(normalize(view * texCoords)) * scaling;
     #endif
 }
