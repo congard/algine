1050c0e6d052b880ee6dafd5af54046a0c40027e
Daniel Bernar <dbcongard@gmail.com>
2019-08-08 19:29:05 +0300

    Update camera.h camera.cpp
    All types of cameras are removed, one camera class is added that inherits Object3D. This class combines all types of cameras. All structures are now classes. Controllers changed

--- include/algine/camera.h
+++ include/algine/camera.h
@@ -4,59 +4,56 @@
 #include <glm/mat4x4.hpp>
 #include <glm/vec3.hpp>
 #include <glm/gtc/quaternion.hpp>
+#include <algine/object3d.h>
 
 namespace algine {
-struct BaseCamera {
-    glm::vec3 pos;
-    glm::mat4 view, projection;
+class Camera: public Object3D {
+public:
+    glm::mat4 m_projection;
     float fov, aspectRatio, near = 1.0f, far = 32.0f;
 
+    Camera(const uint rotatorType = Rotator::RotatorTypeEuler);
+
+    void translate();
+    void updateMatrix();
+    void setFieldOfView(const float fov);
+    void setAspectRatio(const float aspectRatio);
+    void setNear(const float near);
+    void setFar(const float far);
     void perspective();
+
+    glm::mat4 getProjectionMatrix();
+    glm::mat4 getViewMatrix(); // calls Object3D::getMatrix()
+    float getFieldOfView();
+    float getAspectRatio();
+    float getNear();
+    float getFar();
 };
 
-struct Camera : public BaseCamera {
-    glm::vec3 lookPos, up = glm::vec3(0.0f, 1.0f, 0.0f);
-
-    void lookAt();
-};
-
-struct FreeCamera : public BaseCamera {
-    float pitch = 0, yaw = 0, roll = 0;
-    
-    void updateView();
-    
-private:
-    glm::quat qCamera;
-};
-
-struct FPSCamera : public FreeCamera {
-    void updateView();
-};
-
-struct BaseCameraController {
-    BaseCamera *camera;
+class BaseCameraController {
+public:
+    Camera *camera;
+    float sensitivityPitch = 0.0025f, sensitivityYaw = 0.0025f, sensitivityRoll = 0.0025f;
     float speedf = 1.0f, speedb = 1.0f, speedr = 1.0f, speedl = 1.0f;
     
+    void setMousePos(const float x, const float y, const float z = 0);
+    void mouseMove(const float x, const float y, const float z = 0);
     void goForward();
     void goBack();
     void goRight();
     void goLeft();
-};
 
-struct FreeCameraController : public BaseCameraController {
-    float sensitivityPitch = 0.0025f, sensitivityYaw = 0.0025f, sensitivityRoll = 0.0025f;
-    
-    void mouseMove(const float x, const float y, const float z = 0);
-    
-    inline void setMousePos(const float x, const float y, const float z = 0) {
-        lastMousePos = glm::vec3(x, y, z);
-    }
-    
 protected:
     glm::vec3 lastMousePos = glm::vec3(0.0f);
 };
 
-struct FPSCameraController : public FreeCameraController {
+class FreeCameraController : public BaseCameraController {
+public:
+    void mouseMove(const float x, const float y, const float z = 0);
+};
+
+class FPSCameraController : public BaseCameraController {
+public:
     void mouseMove(const float x, const float y, const float z = 0);
 };
 }
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,70 +1,95 @@
 #define GLM_FORCE_CTOR_INIT
 #include <algine/camera.h>
 #include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
 
 namespace algine {
-void BaseCamera::perspective() {
-    projection = glm::perspective(fov, aspectRatio, near, far);
+Camera::Camera(const uint rotatorType) : Object3D (rotatorType) {
+    type = Object3DTypeCamera;
 }
 
-void Camera::lookAt() {
-    view = glm::lookAt(pos, lookPos, up);
+void Camera::translate() {
+    m_translation = glm::translate(glm::mat4(), -v_pos);
 }
 
-void FreeCamera::updateView() {
-    // temporary frame quaternion from pitch, yaw, roll
-    glm::quat qPYR = glm::quat(glm::vec3(pitch, yaw, roll));
-    // reset values
-    pitch = yaw = roll = 0;
-    
-    // update qCamera
-    qCamera = qPYR * qCamera;
-    qCamera = glm::normalize(qCamera);
-    glm::mat4 rotate = glm::mat4_cast(qCamera);
-    
-    glm::mat4 translate = glm::mat4(1.0f);
-    translate = glm::translate(translate, -pos);
-    
-    view = rotate * translate;
+void Camera::updateMatrix() {
+    m_transform = m_rotation * m_translation * m_scaling;
 }
 
-void FPSCamera::updateView() {
-    glm::quat qPitch = glm::angleAxis(pitch, glm::vec3(1, 0, 0));
-    glm::quat qYaw = glm::angleAxis(yaw, glm::vec3(0, 1, 0));
-    glm::quat qRoll = glm::angleAxis(roll,glm::vec3(0, 0, 1));
-    
-    glm::quat orientation = qPitch * qYaw * qRoll;
-    orientation = glm::normalize(orientation);
-    glm::mat4 rotate = glm::mat4_cast(orientation);
- 
-    glm::mat4 translate = glm::mat4(1.0f);
-    translate = glm::translate(translate, -pos);
- 
-    view = rotate * translate;
+void Camera::setFieldOfView(const float fov) {
+    this->fov = fov;
+}
+
+void Camera::setAspectRatio(const float aspectRatio) {
+    this->aspectRatio = aspectRatio;
+}
+
+void Camera::setNear(const float near) {
+    this->near = near;
+}
+
+void Camera::setFar(const float far) {
+    this->far = far;
+}
+
+void Camera::perspective() {
+    m_projection = glm::perspective(fov, aspectRatio, near, far);
+}
+
+glm::mat4 Camera::getProjectionMatrix() {
+    return m_projection;
+}
+
+glm::mat4 Camera::getViewMatrix() {
+    return getMatrix();
+}
+
+float Camera::getFieldOfView() {
+    return fov;
+}
+
+float Camera::getAspectRatio() {
+    return aspectRatio;
+}
+
+float Camera::getNear() {
+    return near;
+}
+
+float Camera::getFar() {
+    return far;
+}
+
+void BaseCameraController::setMousePos(const float x, const float y, const float z) {
+    lastMousePos = glm::vec3(x, y, z);
+}
+
+void BaseCameraController::mouseMove(const float x, const float y, const float z) {
+    std::cerr << "You cannot call mouseMove(" << x << ", " << y << ", " << z << ") directly from BaseCameraController!\n";
 }
 
 void BaseCameraController::goForward() {
-    camera->pos -= glm::vec3(camera->view[0][2], camera->view[1][2], camera->view[2][2]) * speedf;
+    camera->v_pos += camera->getForward() * speedf;
 }
 
 void BaseCameraController::goBack() {
-    camera->pos += glm::vec3(camera->view[0][2], camera->view[1][2], camera->view[2][2]) * speedb;
+    camera->v_pos += camera->getBack() * speedb;
 }
 
 void BaseCameraController::goRight() {
-    camera->pos += glm::vec3(camera->view[0][0], camera->view[1][0], camera->view[2][0]) * speedr;
+    camera->v_pos += camera->getRight() * speedr;
 }
 
 void BaseCameraController::goLeft() {
-    camera->pos -= glm::vec3(camera->view[0][0], camera->view[1][0], camera->view[2][0]) * speedl;
+    camera->v_pos += camera->getLeft() * speedl;
 }
 
 void FreeCameraController::mouseMove(const float x, const float y, const float z) {
     glm::vec3 dCoord = glm::vec3(x, y, z) - lastMousePos;
     
-    ((FreeCamera*)camera)->yaw = dCoord.x * sensitivityYaw;
-    ((FreeCamera*)camera)->pitch = dCoord.y * sensitivityPitch;
-    ((FreeCamera*)camera)->roll = dCoord.z * sensitivityRoll;
+    camera->setYaw(dCoord.x * sensitivityYaw);
+    camera->setYaw(dCoord.y * sensitivityPitch);
+    camera->setYaw(dCoord.z * sensitivityRoll);
  
     lastMousePos = glm::vec3(x, y, z);
 }
@@ -72,9 +97,9 @@ void FreeCameraController::mouseMove(const float x, const float y, const float z
 void FPSCameraController::mouseMove(const float x, const float y, const float z) {
     glm::vec3 dCoord = glm::vec3(x, y, z) - lastMousePos;
     
-    ((FPSCamera*)camera)->yaw += dCoord.x * sensitivityYaw;
-    ((FPSCamera*)camera)->pitch += dCoord.y * sensitivityPitch;
-    ((FPSCamera*)camera)->roll += dCoord.z * sensitivityRoll;
+    camera->rotator->yaw += dCoord.x * sensitivityYaw;
+    camera->rotator->pitch += dCoord.y * sensitivityPitch;
+    camera->rotator->roll += dCoord.z * sensitivityRoll;
  
     lastMousePos = glm::vec3(x, y, z);
 }
