ff3a6926a186e8c872f8d21f08fae416e2b4c0fb
Daniel Bernar <dbcongard@gmail.com>
2019-11-22 19:50:03 +0200

    Add templates.h

---
+++ include/algine/templates.h
@@ -0,0 +1,22 @@
+// TODO: move to the tulz
+
+#ifndef ALGINE_TEMPLATES_H
+#define ALGINE_TEMPLATES_H
+
+#define implementVariadicCreate(T) \
+template<typename...Args> \
+static void create(Args&...args) { \
+    T** arr[] = {&args...}; \
+    for (usize i = 0; i < sizeof...(args); i++) \
+        *arr[i] = new T(); \
+}
+
+#define implementVariadicDestroy(T) \
+template<typename...Args> \
+static void destroy(Args&...args) { \
+    T** arr[] = {&args...}; \
+    for (usize i = 0; i < sizeof...(args); i++) \
+        deletePtr(*arr[i]); \
+}
+
+#endif //ALGINE_TEMPLATES_H
