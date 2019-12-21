39a912702b5f1180af1738343f44b26c07a678b9
Daniel Bernar <dbcongard@gmail.com>
2019-12-21 17:59:17 +0200

    Remove class Object3D, create properties
    Instead of Object3D use rotatable, translatable, scalable, which are properties

--- include/algine/object3d.h
+++ include/algine/object3d.h
@@ -7,68 +7,86 @@
 #include <glm/vec3.hpp>
 
 namespace algine {
-class Object3D {
-private:
-    void swap(Object3D &other);
+/**
+ * Property. Implements functions for rotating an object
+ */
+class rotatable {
+protected:
+    void swap(rotatable &other);
+
 public:
-    enum {
-        Object3DTypePlain,
-        Object3DTypeModel,
-        Object3DTypeCamera
-    };
-
-    Rotator *rotator = nullptr;
-    glm::mat4 m_rotation, m_translation, m_scaling;
-    glm::mat4 m_transform;
-    glm::vec3 v_pos, v_scale;
-    uint type = Object3DTypePlain;
-
-    explicit Object3D(uint rotatorType = Rotator::RotatorTypeSimple);
-    Object3D(const Object3D &src); // copy constructor
-    Object3D(Object3D &&src) noexcept; // move constructor
-    Object3D& operator=(const Object3D &rhs); // copy operator
-    Object3D& operator=(Object3D &&rhs) noexcept; // move operator
-    ~Object3D();
+    explicit rotatable(uint rotatorType = Rotator::RotatorTypeSimple);
+    rotatable(const rotatable &src); // copy constructor
+    rotatable(rotatable &&src) noexcept; // move constructor
+    rotatable& operator=(const rotatable &rhs); // copy operator
+    rotatable& operator=(rotatable &&rhs) noexcept; // move operator
+    ~rotatable();
 
     void setPitch(float pitch);
     void setYaw(float yaw);
     void setRoll(float roll);
     void setRotate(float pitch, float yaw, float roll);
     void rotate();
+
+    glm::vec3 getRotate() const;
+    float getPitch() const;
+    float getYaw() const;
+    float getRoll() const;
+    glm::mat4 getRotationMatrix() const;
+    glm::vec3 getForward() const;
+    glm::vec3 getBack() const;
+    glm::vec3 getRight() const;
+    glm::vec3 getLeft() const;
+
+public:
+    Rotator *rotator = nullptr;
+    glm::mat4 m_rotation;
+};
+
+/**
+ * Property. Implements functions for translating an object
+ */
+class translatable {
+public:
     void setPos(float x, float y, float z);
     void setPos(const glm::vec3 &pos);
     void setX(float x);
     void setY(float y);
     void setZ(float z);
-    void translate();
+    virtual void translate();
+
+    glm::vec3 getPos() const;
+    float getX() const;
+    float getY() const;
+    float getZ() const;
+    glm::mat4 getTranslationMatrix() const;
+
+public:
+    glm::vec3 m_pos;
+    glm::mat4 m_translation;
+};
+
+/**
+ * Property. Implements functions for scaling an object
+ */
+class scalable {
+public:
     void setScale(float x, float y, float z);
     void setScale(const glm::vec3 &scale);
     void setScaleX(float x);
     void setScaleY(float y);
     void setScaleZ(float z);
     void scale();
-    void updateMatrix();
 
-    glm::vec3 getPosition();
-    float getX();
-    float getY();
-    float getZ();
-    glm::vec3 getScale();
-    float getScaleX();
-    float getScaleY();
-    float getScaleZ();
-    glm::vec3 getRotate();
-    float getPitch();
-    float getYaw();
-    float getRoll();
-    glm::mat4 getRotationMatrix();
-    glm::mat4 getTranslationMatrix();
-    glm::mat4 getScalingMatrix();
-    glm::mat4 getMatrix();
-    glm::vec3 getForward();
-    glm::vec3 getBack();
-    glm::vec3 getRight();
-    glm::vec3 getLeft();
+    glm::vec3 getScale() const;
+    float getScaleX() const;
+    float getScaleY() const;
+    float getScaleZ() const;
+    glm::mat4 getScalingMatrix() const;
+
+public:
+    glm::vec3 m_scale;
+    glm::mat4 m_scaling;
 };
 }
 
--- src/object3d.cpp
+++ src/object3d.cpp
@@ -5,206 +5,186 @@
 #include <algorithm>
 
 namespace algine {
-Object3D::Object3D(const uint rotatorType) {
+void rotatable::swap(algine::rotatable &other) {
+    std::swap(m_rotation, other.m_rotation);
+    std::swap(rotator, other.rotator);
+}
+
+rotatable::rotatable(const uint rotatorType) {
     rotator = Rotator::create(rotatorType);
 }
 
-Object3D::Object3D(const Object3D &src) {
+rotatable::rotatable(const rotatable &src) {
     m_rotation = src.m_rotation;
-    m_translation = src.m_translation;
-    m_scaling = src.m_scaling;
-    m_transform = src.m_transform;
-    v_pos = src.v_pos;
-    v_scale = src.v_scale;
-    type = src.type;
     rotator = Rotator::create(src.rotator->type);
 }
 
-Object3D::Object3D(Object3D &&src) noexcept {
+rotatable::rotatable(rotatable &&src) noexcept {
     src.swap(*this);
 }
 
-Object3D& Object3D::operator=(const Object3D &rhs) {
+rotatable& rotatable::operator=(const rotatable &rhs) {
     if (&rhs != this)
-        Object3D(rhs).swap(*this);
+        rotatable(rhs).swap(*this);
     return *this;
 }
 
-Object3D& Object3D::operator=(Object3D &&rhs) noexcept {
+rotatable& rotatable::operator=(rotatable &&rhs) noexcept {
     rhs.swap(*this);
     return *this;
 }
 
-Object3D::~Object3D() {
+rotatable::~rotatable() {
     deletePtr(rotator)
 }
 
-void Object3D::swap(Object3D &other) {
-    std::swap(m_rotation, other.m_rotation);
-    std::swap(m_translation, other.m_translation);
-    std::swap(m_scaling, other.m_scaling);
-    std::swap(m_transform, other.m_transform);
-    std::swap(v_pos, other.v_pos);
-    std::swap(v_scale, other.v_scale);
-    std::swap(type, other.type);
-    std::swap(rotator, other.rotator);
-}
-
-void Object3D::setPitch(const float pitch) {
+void rotatable::setPitch(const float pitch) {
     rotator->pitch = pitch;
 }
 
-void Object3D::setYaw(const float yaw) {
+void rotatable::setYaw(const float yaw) {
     rotator->yaw = yaw;
 }
 
-void Object3D::setRoll(const float roll) {
+void rotatable::setRoll(const float roll) {
     rotator->roll = roll;
 }
 
-void Object3D::setRotate(const float pitch, const float yaw, const float roll) {
+void rotatable::setRotate(const float pitch, const float yaw, const float roll) {
     rotator->pitch = pitch;
     rotator->yaw = yaw;
     rotator->roll = roll;
 }
 
-void Object3D::rotate() {
+void rotatable::rotate() {
     rotator->rotate(m_rotation);
 }
 
-void Object3D::setPos(const float x, const float y, const float z) {
-    v_pos.x = x;
-    v_pos.y = y;
-    v_pos.z = z;
-}
-
-void Object3D::setPos(const glm::vec3 &pos) {
-    v_pos = pos;
-}
-
-void Object3D::setX(const float x) {
-    v_pos.x = x;
-}
-
-void Object3D::setY(const float y) {
-    v_pos.y = y;
-}
-
-void Object3D::setZ(const float z) {
-    v_pos.z = z;
-}
-
-void Object3D::translate() {
-    m_translation = glm::translate(glm::mat4(), v_pos);
-}
-
-void Object3D::setScale(const float x, const float y, const float z) {
-    v_scale.x = x;
-    v_scale.y = y;
-    v_scale.z = z;
-}
-
-void Object3D::setScale(const glm::vec3 &scale) {
-    v_scale = scale;
-}
-
-void Object3D::setScaleX(const float x) {
-    v_scale.x = x;
-}
-
-void Object3D::setScaleY(const float y) {
-    v_scale.y = y;
-}
-
-void Object3D::setScaleZ(const float z) {
-    v_scale.z = z;
-}
-
-void Object3D::scale() {
-    m_scaling = glm::scale(glm::mat4(), v_scale);
-}
-
-void Object3D::updateMatrix() {
-    m_transform = m_translation * m_rotation * m_scaling;
-}
-
-glm::vec3 Object3D::getPosition() {
-    return v_pos;
-}
-
-float Object3D::getX() {
-    return v_pos.x;
-}
-
-float Object3D::getY() {
-    return v_pos.y;
-}
-
-float Object3D::getZ() {
-    return v_pos.z;
-}
-
-glm::vec3 Object3D::getScale() {
-    return v_scale;
-}
-
-float Object3D::getScaleX() {
-    return v_scale.x;
-}
-
-float Object3D::getScaleY() {
-    return v_scale.y;
-}
-
-float Object3D::getScaleZ() {
-    return v_scale.z;
-}
-
-glm::vec3 Object3D::getRotate() {
+glm::vec3 rotatable::getRotate() const {
     return glm::vec3(rotator->pitch, rotator->yaw, rotator->roll);
 }
 
-float Object3D::getPitch() {
+float rotatable::getPitch() const {
     return rotator->pitch;
 }
 
-float Object3D::getYaw() {
+float rotatable::getYaw() const {
     return rotator->yaw;
 }
 
-float Object3D::getRoll() {
+float rotatable::getRoll() const {
     return rotator->roll;
 }
 
-glm::mat4 Object3D::getRotationMatrix() {
+glm::mat4 rotatable::getRotationMatrix() const {
     return m_rotation;
 }
 
-glm::mat4 Object3D::getTranslationMatrix() {
-    return m_translation;
-}
-
-glm::mat4 Object3D::getScalingMatrix() {
-    return m_scaling;
-}
-
-glm::mat4 Object3D::getMatrix() {
-    return m_transform;
-}
-
-glm::vec3 Object3D::getForward() {
+glm::vec3 rotatable::getForward() const {
     return -glm::vec3(m_rotation[0][2], m_rotation[1][2], m_rotation[2][2]);
 }
 
-glm::vec3 Object3D::getBack() {
+glm::vec3 rotatable::getBack() const {
     return -getForward();
 }
 
-glm::vec3 Object3D::getRight() {
+glm::vec3 rotatable::getRight() const {
     return glm::vec3(m_rotation[0][0], m_rotation[1][0], m_rotation[2][0]);
 }
 
-glm::vec3 Object3D::getLeft() {
+glm::vec3 rotatable::getLeft() const {
     return -getRight();
+}
+
+void translatable::setPos(const float x, const float y, const float z) {
+    m_pos.x = x;
+    m_pos.y = y;
+    m_pos.z = z;
+}
+
+void translatable::setPos(const glm::vec3 &pos) {
+    m_pos = pos;
+}
+
+void translatable::setX(const float x) {
+    m_pos.x = x;
+}
+
+void translatable::setY(const float y) {
+    m_pos.y = y;
+}
+
+void translatable::setZ(const float z) {
+    m_pos.z = z;
+}
+
+void translatable::translate() {
+    m_translation = glm::translate(glm::mat4(), m_pos);
+}
+
+glm::vec3 translatable::getPos() const {
+    return m_pos;
+}
+
+float translatable::getX() const {
+    return m_pos.x;
+}
+
+float translatable::getY() const {
+    return m_pos.y;
+}
+
+float translatable::getZ() const {
+    return m_pos.z;
+}
+
+glm::mat4 translatable::getTranslationMatrix() const {
+    return m_translation;
+}
+
+void scalable::setScale(const float x, const float y, const float z) {
+    m_scale.x = x;
+    m_scale.y = y;
+    m_scale.z = z;
+}
+
+void scalable::setScale(const glm::vec3 &scale) {
+    m_scale = scale;
+}
+
+void scalable::setScaleX(const float x) {
+    m_scale.x = x;
+}
+
+void scalable::setScaleY(const float y) {
+    m_scale.y = y;
+}
+
+void scalable::setScaleZ(const float z) {
+    m_scale.z = z;
+}
+
+void scalable::scale() {
+    m_scaling = glm::scale(glm::mat4(), m_scale);
+}
+
+glm::vec3 scalable::getScale() const {
+    return m_scale;
+}
+
+float scalable::getScaleX() const {
+    return m_scale.x;
+}
+
+float scalable::getScaleY() const {
+    return m_scale.y;
+}
+
+float scalable::getScaleZ() const {
+    return m_scale.z;
+}
+
+glm::mat4 scalable::getScalingMatrix() const {
+    return m_scaling;
 }
 }
