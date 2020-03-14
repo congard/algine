c0b22b1825d8fff583f8d1faf4b87e7966992312
Daniel Bernar <dbcongard@gmail.com>
2020-03-14 12:19:30 +0200

    Create bone system module

---
+++ resources/modules/BoneSystem.glsl
@@ -0,0 +1,28 @@
+/**
+ * Bone System
+ * It is module, not shader
+ * Add it in your shader via pragma
+ * algine include "modules/BoneSystem.glsl"
+ */
+
+uniform mat4 bones[MAX_BONES];
+uniform int boneAttribsPerVertex = 0;
+
+in vec4 inBoneWeights[MAX_BONE_ATTRIBS_PER_VERTEX];
+in ivec4 inBoneIds[MAX_BONE_ATTRIBS_PER_VERTEX];
+
+#define isBonesPresent() (boneAttribsPerVertex > 0)
+#define getBonesCount() boneAttribsPerVertex
+
+mat4 getBoneTransformMatrix() {
+    mat4 finalTransform = mat4(0.0);
+
+    for (int i = 0; i < boneAttribsPerVertex; i++) {
+        finalTransform += bones[inBoneIds[i].x] * inBoneWeights[i].x;
+        finalTransform += bones[inBoneIds[i].y] * inBoneWeights[i].y;
+        finalTransform += bones[inBoneIds[i].z] * inBoneWeights[i].z;
+        finalTransform += bones[inBoneIds[i].w] * inBoneWeights[i].w;
+    }
+
+    return finalTransform;
+}
