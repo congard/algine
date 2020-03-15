520012841f3ad5e9e3a2f96330024002a7440584
Daniel Bernar <dbcongard@gmail.com>
2020-03-15 15:06:48 +0200

    Use blend bloom module

--- resources/shaders/blend/fragment.glsl
+++ resources/shaders/blend/fragment.glsl
@@ -1,6 +1,6 @@
-// Algine blend fragment shader
-
 #version 330
+
+#pragma algine include "modules/BlendBloom.glsl"
 
 out vec3 fragColor;
 in vec2 texCoord;
@@ -11,14 +11,12 @@ uniform float exposure;
 uniform float gamma;
 
 void main() {
-	#ifdef ALGINE_BLOOM_TYPE_ADD // additive blending
-		vec3 color = texture(bloom, texCoord).rgb + texture(image, texCoord).rgb;
-	#elif defined ALGINE_BLOOM_TYPE_SCREEN // screen blending
-		vec3 color = 1 - (1 - texture(bloom, texCoord).rgb) * (1 - texture(image, texCoord).rgb);
-	#else
-		vec3 color = texture(image, texCoord).rgb;
-	#endif
-
+	vec3 color;
+	if (isBloomEnabled())
+		color = blendBloom(texture(bloom, texCoord).rgb, texture(image, texCoord).rgb);
+	else
+		color = texture(image, texCoord).rgb;
+	
 	fragColor = vec3(1.0) - exp(-color * exposure); // tone mapping
 	fragColor = pow(fragColor, vec3(1.0 / gamma)); // gamma correction
 }
