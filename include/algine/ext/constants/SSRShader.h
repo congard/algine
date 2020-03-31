d368b40c0dcbcd3a3fb509d6ffd219988e71eaa8
Daniel Bernar <dbcongard@gmail.com>
2020-03-31 18:11:41 +0300

    SSR material props

--- include/algine/ext/constants/SSRShader.h
+++ include/algine/ext/constants/SSRShader.h
@@ -6,22 +6,31 @@
 #define constant(name, val) constexpr char name[] = val;
 
 namespace algine {
-    namespace SSRShader {
-        namespace Vars {
-            constant(BaseImage, "baseImage")
-            constant(NormalMap, "normalMap")
-            constant(SSRValuesMap, "ssrValuesMap")
-            constant(PositionMap, "positionMap")
-            constant(ProjectionMatrix, "projection")
-            constant(ViewMatrix, "view")
-            constant(SkyColor, "skyColor")
-            constant(BinarySearchCount, "binarySearchCount")
-            constant(RayMarchCount, "rayMarchCount")
-            constant(RayStep, "rayStep")
-            constant(LLimiter, "LLimiter")
-            constant(MinRayStep, "minRayStep")
-        }
+namespace SSRShader {
+    namespace Vars {
+        constant(BaseImage, "baseImage")
+        constant(NormalMap, "normalMap")
+        constant(SSRValuesMap, "ssrValuesMap")
+        constant(PositionMap, "positionMap")
+        constant(ProjectionMatrix, "projection")
+        constant(ViewMatrix, "view")
+        constant(SkyColor, "skyColor")
+        constant(BinarySearchCount, "binarySearchCount")
+        constant(RayMarchCount, "rayMarchCount")
+        constant(RayStep, "rayStep")
+        constant(LLimiter, "LLimiter")
+        constant(MinRayStep, "minRayStep")
     }
+}
+
+namespace Module {
+namespace Material {
+    namespace Vars {
+        constant(ReflectionStrengthTex, "material.reflectionStrength")
+        constant(JitterTex, "material.jitter")
+    }
+}
+}
 }
 
 #undef constant
---
+++ resources/shaders/SSR.Material.glsl
@@ -0,0 +1,8 @@
+/**
+ * SSR Material
+ * Custom properties for default Material structure
+ * Include it in your Material.custom.glsl
+ */
+
+sampler2D reflectionStrength;
+sampler2D jitter;
