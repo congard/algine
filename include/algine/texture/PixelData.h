71a81cee17cb71e18a5a1ec7158303d3d89f5938
Daniel Bernar <dbcongard@gmail.com>
2020-04-15 11:33:53 +0300

    Add PixelData.h

---
+++ include/algine/texture/PixelData.h
@@ -0,0 +1,15 @@
+#ifndef ALGINE_PIXELDATA_H
+#define ALGINE_PIXELDATA_H
+
+#include <tulz/array.h>
+#include <algine/types.h>
+
+namespace algine {
+struct PixelData {
+    uint width, height;
+    uint componentsCount;
+    tulz::Array<float> pixels;
+};
+}
+
+#endif //ALGINE_PIXELDATA_H
