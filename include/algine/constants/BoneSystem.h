67c834315cbe505da264d507ad090b307710eb19
Daniel Bernar <dbcongard@gmail.com>
2020-07-25 18:56:46 +0300

    Update BoneSystem constants

--- include/algine/constants/BoneSystem.h
+++ include/algine/constants/BoneSystem.h
@@ -1,24 +1,30 @@
-#ifndef ALGINE_BONESYSTEM_H
-#define ALGINE_BONESYSTEM_H
+#ifndef ALGINE_BONESYSTEM_CONSTANTS_H
+#define ALGINE_BONESYSTEM_CONSTANTS_H
 
 #define constant(name, val) constexpr char name[] = val;
 
 namespace algine {
-    namespace BoneSystem {
-        namespace Settings {
-            constant(MaxBoneAttribsPerVertex, "MAX_BONE_ATTRIBS_PER_VERTEX")
-            constant(MaxBones, "MAX_BONES")
-        }
+namespace Module {
+namespace BoneSystem {
+    namespace Settings {
+        constant(MaxBoneAttribsPerVertex, "MAX_BONE_ATTRIBS_PER_VERTEX")
+        constant(MaxBones, "MAX_BONES")
+    }
 
-        namespace Vars {
-            constant(InBoneIds, "inBoneIds[0]") // integer
-            constant(InBoneWeights, "inBoneWeights[0]")
-            constant(Bones, "bones[0]") // zero element, so ShaderProgram.getLocation(Bones) + i, not bones[i]
-            constant(BoneAttribsPerVertex, "boneAttribsPerVertex") // bonesPerVertex / 4 + (bonesPerVertex % 4 == 0 ? 0 : 1)
+    namespace Vars {
+        constant(InBoneIds, "inBoneIds[0]") // integer
+        constant(InBoneWeights, "inBoneWeights[0]")
+
+        namespace Block {
+            constant(Name, "BoneSystem")
+            constant(Bones, "bones")
+            constant(BoneAttribsPerVertex, "boneAttribsPerVertex")
         }
     }
+}
+}
 }
 
 #undef constant
 
-#endif //ALGINE_BONESYSTEM_H
+#endif //ALGINE_BONESYSTEM_CONSTANTS_H
