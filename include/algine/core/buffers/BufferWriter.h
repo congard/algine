6a6600189d0963dcf8f7734b90c497bcfd4e372b
Daniel Bernar <dbcongard@gmail.com>
2020-08-15 12:08:25 +0300

    Add BufferWriter

---
+++ include/algine/core/buffers/BufferWriter.h
@@ -0,0 +1,50 @@
+#ifndef ALGINE_BUFFERWRITER_H
+#define ALGINE_BUFFERWRITER_H
+
+#include <algine/core/buffers/Buffer.h>
+
+#include <tulz/Array.h>
+#include <vector>
+
+namespace algine {
+class BufferWriter {
+public:
+    BufferWriter();
+    explicit BufferWriter(Buffer *buffer);
+
+    template<typename T>
+    inline void write(uint offset, const std::vector<T> &data) {
+        write(offset, data.size() * sizeof(T), data.data());
+    }
+
+    template<typename T>
+    inline void write(uint offset, const tulz::Array<T> &data) {
+        write(offset, data.size() * sizeof(T), data.array());
+    }
+
+    template<typename T>
+    inline typename std::enable_if<std::is_fundamental<T>::value>::type write(uint offset, T data)
+    {
+        write(offset, sizeof(T), &data);
+    }
+
+    template<typename T>
+    inline typename std::enable_if<!std::is_fundamental<T>::value>::type write(uint offset, const T &data)
+    {
+        write(offset, sizeof(T), &data);
+    }
+
+    void begin();
+    void end();
+    void write(uint offset, uint size, const void *data);
+
+    void setBuffer(Buffer *buffer);
+
+    Buffer* getBuffer() const;
+
+private:
+    Buffer *m_buffer = nullptr;
+};
+}
+
+#endif //ALGINE_BUFFERWRITER_H
---
+++ src/core/buffers/BufferWriter.cpp
@@ -0,0 +1,31 @@
+#include <algine/core/buffers/BufferWriter.h>
+
+using namespace std;
+
+namespace algine {
+BufferWriter::BufferWriter() = default;
+
+BufferWriter::BufferWriter(Buffer *buffer) {
+    m_buffer = buffer;
+}
+
+void BufferWriter::begin() {
+    m_buffer->bind();
+}
+
+void BufferWriter::end() {
+    m_buffer->unbind();
+}
+
+void BufferWriter::write(uint offset, uint size, const void *data) {
+    m_buffer->updateData(offset, size, data);
+}
+
+void BufferWriter::setBuffer(Buffer *buffer) {
+    m_buffer = buffer;
+}
+
+Buffer* BufferWriter::getBuffer() const {
+    return m_buffer;
+}
+}
