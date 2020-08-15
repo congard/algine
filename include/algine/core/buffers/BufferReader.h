0bf85c277366df41c6cf83b8789230d93d4c5378
Daniel Bernar <dbcongard@gmail.com>
2020-08-15 15:18:03 +0300

    Add BufferReader

---
+++ include/algine/core/buffers/BufferReader.h
@@ -0,0 +1,34 @@
+#ifndef ALGINE_BUFFERREADER_H
+#define ALGINE_BUFFERREADER_H
+
+#include <algine/core/buffers/Buffer.h>
+
+#include <tulz/Array.h>
+
+namespace algine {
+class BufferReader {
+public:
+    BufferReader();
+    explicit BufferReader(Buffer *buffer);
+
+    void begin();
+    void end();
+
+    tulz::Array<byte> read(uint offset, uint size);
+
+    template<typename T>
+    inline T read(uint offset) {
+        // return copy of the object from bytes
+        return *reinterpret_cast<T*>(read(offset, sizeof(T)).array());
+    }
+
+    void setBuffer(Buffer *buffer);
+
+    Buffer* getBuffer() const;
+
+private:
+    Buffer *m_buffer = nullptr;
+};
+}
+
+#endif //ALGINE_BUFFERREADER_H
---
+++ src/core/buffers/BufferReader.cpp
@@ -0,0 +1,31 @@
+#include <algine/core/buffers/BufferReader.h>
+
+using namespace tulz;
+
+namespace algine {
+BufferReader::BufferReader() = default;
+
+BufferReader::BufferReader(Buffer *buffer) {
+    m_buffer = buffer;
+}
+
+void BufferReader::begin() {
+    m_buffer->bind();
+}
+
+void BufferReader::end() {
+    m_buffer->unbind();
+}
+
+Array<byte> BufferReader::read(uint offset, uint size) {
+    return m_buffer->getData(offset, size);
+}
+
+void BufferReader::setBuffer(Buffer *buffer) {
+    m_buffer = buffer;
+}
+
+Buffer* BufferReader::getBuffer() const {
+    return m_buffer;
+}
+}
