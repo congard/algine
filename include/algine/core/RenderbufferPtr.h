314548c3b1b2a71997efd46c42cce0220572a37c
Daniel Bernar <dbcongard@gmail.com>
2020-09-15 17:08:10 +0300

    Add RenderbufferPtr

---
+++ include/algine/core/RenderbufferPtr.h
@@ -0,0 +1,12 @@
+#ifndef ALGINE_RENDERBUFFERPTR_H
+#define ALGINE_RENDERBUFFERPTR_H
+
+#include <memory>
+
+namespace algine {
+class Renderbuffer;
+
+typedef std::shared_ptr<Renderbuffer> RenderbufferPtr;
+}
+
+#endif //ALGINE_RENDERBUFFERPTR_H
