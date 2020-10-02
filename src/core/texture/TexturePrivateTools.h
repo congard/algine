b37ea47c12287bb0ed8ea13447c16e5f9e6a7dac
Daniel Bernar <dbcongard@gmail.com>
2020-10-02 17:52:50 +0200

    Add TexturePrivateTools.h
    Functions common to multiple cpp files

---
+++ src/core/texture/TexturePrivateTools.h
@@ -0,0 +1,19 @@
+#ifndef ALGINE_TEXTUREPRIVATETOOLS_H
+#define ALGINE_TEXTUREPRIVATETOOLS_H
+
+#include <algine/types.h>
+
+#include <GL/glew.h>
+
+namespace algine::TexturePrivateTools {
+inline uint getTexParam(const uint target, const uint name) {
+    constexpr uint mipLevel = 0;
+
+    int value;
+    glGetTexLevelParameteriv(target, mipLevel, name, &value);
+
+    return value;
+}
+}
+
+#endif //ALGINE_TEXTUREPRIVATETOOLS_H
