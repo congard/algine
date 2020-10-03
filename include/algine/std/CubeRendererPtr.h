5b958255046b1d081268272adc96a23cbc98c8b3
Daniel Bernar <dbcongard@gmail.com>
2020-10-03 19:33:29 +0200

    Add CubeRendererPtr.h and QuadRendererPtr.h

---
+++ include/algine/std/CubeRendererPtr.h
@@ -0,0 +1,12 @@
+#ifndef ALGINE_CUBERENDERERPTR_H
+#define ALGINE_CUBERENDERERPTR_H
+
+#include <memory>
+
+namespace algine {
+class CubeRenderer;
+
+typedef std::shared_ptr<CubeRenderer> CubeRendererPtr;
+}
+
+#endif //ALGINE_CUBERENDERERPTR_H
---
+++ include/algine/std/QuadRendererPtr.h
@@ -0,0 +1,12 @@
+#ifndef ALGINE_QUADRENDERERPTR_H
+#define ALGINE_QUADRENDERERPTR_H
+
+#include <memory>
+
+namespace algine {
+class QuadRenderer;
+
+typedef std::shared_ptr<QuadRenderer> QuadRendererPtr;
+}
+
+#endif //ALGINE_QUADRENDERERPTR_H
