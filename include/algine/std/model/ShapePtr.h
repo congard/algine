911b1ceebed0a0af9a2a9d9131d00b14a253fbb0
Daniel Bernar <dbcongard@gmail.com>
2020-10-20 17:57:05 +0200

    Add ShapePtr.h

---
+++ include/algine/std/model/ShapePtr.h
@@ -0,0 +1,12 @@
+#ifndef ALGINE_SHAPEPTR_H
+#define ALGINE_SHAPEPTR_H
+
+#include <algine/core/Ptr.h>
+
+namespace algine {
+class Shape;
+
+typedef Ptr<Shape> ShapePtr;
+}
+
+#endif //ALGINE_SHAPEPTR_H
