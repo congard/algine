4b0453927de5f575688d42c82be6041e117237f3
Daniel Bernar <dbcongard@gmail.com>
2020-12-06 15:05:31 +0200

    Add ModelPtr.h

---
+++ include/algine/std/model/ModelPtr.h
@@ -0,0 +1,12 @@
+#ifndef ALGINE_MODELPTR_H
+#define ALGINE_MODELPTR_H
+
+#include <algine/core/Ptr.h>
+
+namespace algine {
+class Model;
+
+typedef Ptr<Model> ModelPtr;
+}
+
+#endif //ALGINE_MODELPTR_H
