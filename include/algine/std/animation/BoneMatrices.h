c271f9105be07b35769400eececf26c06c5cec72
Daniel Bernar <dbcongard@gmail.com>
2020-12-16 18:00:06 +0200

    Add BoneMatrix.h & BoneMatrices.h

---
+++ include/algine/std/animation/BoneMatrices.h
@@ -0,0 +1,12 @@
+#ifndef ALGINE_BONEMATRICES_H
+#define ALGINE_BONEMATRICES_H
+
+#include <algine/std/animation/BoneMatrix.h>
+
+#include <vector>
+
+namespace algine {
+using BoneMatrices = std::vector<BoneMatrix>;
+}
+
+#endif //ALGINE_BONEMATRICES_H
---
+++ include/algine/std/animation/BoneMatrix.h
@@ -0,0 +1,10 @@
+#ifndef ALGINE_BONEMATRIX_H
+#define ALGINE_BONEMATRIX_H
+
+#include <glm/mat4x4.hpp>
+
+namespace algine {
+using BoneMatrix = glm::mat4;
+}
+
+#endif //ALGINE_BONEMATRIX_H
