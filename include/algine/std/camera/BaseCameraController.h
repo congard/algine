51e251c03bab05f49621c342792e97faac916cca
Daniel Bernar <dbcongard@gmail.com>
2020-12-23 17:48:29 +0200

    Add getters and setters & use initializer list

--- include/algine/std/camera/BaseCameraController.h
+++ include/algine/std/camera/BaseCameraController.h
@@ -2,25 +2,53 @@
 #define ALGINE_BASECAMERACONTROLLER_H
 
 #include <algine/std/camera/Camera.h>
+
 #include <glm/vec3.hpp>
+
+// TODO: merge with Camera - eliminate Controller classes
 
 namespace algine {
 class BaseCameraController {
 public:
-    virtual void mouseMove(float x, float y, float z = 0) = 0;
+    BaseCameraController();
+
+    virtual void mouseMove(float x, float y, float z) = 0;
+    void mouseMove(float x, float y);
     void setMousePos(float x, float y, float z = 0);
     void goForward();
     void goBack();
     void goRight();
     void goLeft();
 
-public:
-    Camera *camera;
-    float sensitivityPitch = 0.0025f, sensitivityYaw = 0.0025f, sensitivityRoll = 0.0025f;
-    float speedf = 1.0f, speedb = 1.0f, speedr = 1.0f, speedl = 1.0f;
+    void setCamera(Camera *camera);
+
+    void setSensitivityPitch(float sensitivityPitch);
+    void setSensitivityYaw(float sensitivityYaw);
+    void setSensitivityRoll(float sensitivityRoll);
+
+    void setSpeedForward(float speedForward);
+    void setSpeedBack(float speedBack);
+    void setSpeedRight(float speedRight);
+    void setSpeedLeft(float speedLeft);
+
+    Camera* getCamera() const;
+
+    float getSensitivityPitch() const;
+    float getSensitivityYaw() const;
+    float getSensitivityRoll() const;
+
+    float getSpeedForward() const;
+    float getSpeedBack() const;
+    float getSpeedRight() const;
+    float getSpeedLeft() const;
 
 protected:
-    glm::vec3 lastMousePos = glm::vec3(0.0f);
+    Camera *m_camera;
+    float m_sensitivityPitch, m_sensitivityYaw, m_sensitivityRoll;
+    float m_speedForward, m_speedBack, m_speedRight, m_speedLeft;
+
+protected:
+    glm::vec3 m_lastMousePos;
 };
 }
 
--- src/std/camera/BaseCameraController.cpp
+++ src/std/camera/BaseCameraController.cpp
@@ -2,23 +2,104 @@
 #include <algine/std/camera/BaseCameraController.h>
 
 namespace algine {
-void BaseCameraController::setMousePos(const float x, const float y, const float z) {
-    lastMousePos = glm::vec3(x, y, z);
+BaseCameraController::BaseCameraController()
+    : m_camera(nullptr),
+      m_sensitivityPitch(0.0025f),
+      m_sensitivityYaw(0.0025f),
+      m_sensitivityRoll(0.0025f),
+      m_speedForward(1.0f),
+      m_speedBack(1.0f),
+      m_speedRight(1.0f),
+      m_speedLeft(1.0f),
+      m_lastMousePos(0.0f) {}
+
+void BaseCameraController::mouseMove(float x, float y) {
+    mouseMove(x, y, 0);
+}
+
+void BaseCameraController::setMousePos(float x, float y, float z) {
+    m_lastMousePos = glm::vec3(x, y, z);
 }
 
 void BaseCameraController::goForward() {
-    camera->m_pos += camera->getForward() * speedf;
+    m_camera->m_pos += m_camera->getForward() * m_speedForward;
 }
 
 void BaseCameraController::goBack() {
-    camera->m_pos += camera->getBack() * speedb;
+    m_camera->m_pos += m_camera->getBack() * m_speedBack;
 }
 
 void BaseCameraController::goRight() {
-    camera->m_pos += camera->getRight() * speedr;
+    m_camera->m_pos += m_camera->getRight() * m_speedRight;
 }
 
 void BaseCameraController::goLeft() {
-    camera->m_pos += camera->getLeft() * speedl;
+    m_camera->m_pos += m_camera->getLeft() * m_speedLeft;
 }
+
+void BaseCameraController::setCamera(Camera *camera) {
+    m_camera = camera;
+}
+
+void BaseCameraController::setSensitivityPitch(float sensitivityPitch) {
+    m_sensitivityPitch = sensitivityPitch;
+}
+
+void BaseCameraController::setSensitivityYaw(float sensitivityYaw) {
+    m_sensitivityYaw = sensitivityYaw;
+}
+
+void BaseCameraController::setSensitivityRoll(float sensitivityRoll) {
+    m_sensitivityRoll = sensitivityRoll;
+}
+
+void BaseCameraController::setSpeedForward(float speedForward) {
+    m_speedForward = speedForward;
+}
+
+void BaseCameraController::setSpeedBack(float speedBack) {
+    m_speedBack = speedBack;
+}
+
+void BaseCameraController::setSpeedRight(float speedRight) {
+    m_speedRight = speedRight;
+}
+
+void BaseCameraController::setSpeedLeft(float speedLeft) {
+    m_speedLeft = speedLeft;
+}
+
+Camera* BaseCameraController::getCamera() const {
+    return m_camera;
+}
+
+float BaseCameraController::getSensitivityPitch() const {
+    return m_sensitivityPitch;
+}
+
+float BaseCameraController::getSensitivityYaw() const {
+    return m_sensitivityYaw;
+}
+
+float BaseCameraController::getSensitivityRoll() const {
+    return m_sensitivityRoll;
+}
+
+float BaseCameraController::getSpeedForward() const {
+    return m_speedForward;
+}
+
+float BaseCameraController::getSpeedBack() const {
+    return m_speedBack;
+}
+
+float BaseCameraController::getSpeedRight() const {
+    return m_speedRight;
+}
+
+float BaseCameraController::getSpeedLeft() const {
+    return m_speedLeft;
+}
+
+
 }
