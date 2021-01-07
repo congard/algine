6114026ae706b7ab7bdb4b5f30fbf16bd5fe899d
Daniel Bernar <dbcongard@gmail.com>
2021-01-07 15:21:54 +0200

    Merge FPSCamera and FreeCamera with Camera & remove useless methods
    * Removed: setSensitivityXXX, setSpeedXXX, getSensitivityXXX, getSpeedXXX
    * goXXX behaviour changed: changePos + translate + updateMatrix instead of just changePos

--- include/algine/std/camera/Camera.h
+++ include/algine/std/camera/Camera.h
@@ -10,30 +10,27 @@
 namespace algine {
 class Camera: public Rotatable, public Translatable, public Scalable {
 public:
-    virtual void changeRotation(const glm::vec3 &dAngles) = 0;
+    enum class Type {
+        FPS,
+        Free
+    };
+
+public:
+    explicit Camera(Type type = Type::FPS);
 
     void translate() override;
     void updateMatrix();
 
-    void goForward();
-    void goBack();
-    void goRight();
-    void goLeft();
+    void goForward(float k = 1.0f);
+    void goBack(float k = 1.0f);
+    void goRight(float k = 1.0f);
+    void goLeft(float k = 1.0f);
 
     void setFieldOfView(float fov);
     void setAspectRatio(float aspectRatio);
     void setNear(float near);
     void setFar(float far);
     void perspective();
-
-    void setSensitivityPitch(float sensitivityPitch);
-    void setSensitivityYaw(float sensitivityYaw);
-    void setSensitivityRoll(float sensitivityRoll);
-
-    void setSpeedForward(float speedForward);
-    void setSpeedBack(float speedBack);
-    void setSpeedRight(float speedRight);
-    void setSpeedLeft(float speedLeft);
 
     glm::mat4 getProjectionMatrix() const;
     glm::mat4 getViewMatrix() const;
@@ -42,23 +39,9 @@ public:
     float getNear() const;
     float getFar() const;
 
-    float getSensitivityPitch() const;
-    float getSensitivityYaw() const;
-    float getSensitivityRoll() const;
-
-    float getSpeedForward() const;
-    float getSpeedBack() const;
-    float getSpeedRight() const;
-    float getSpeedLeft() const;
-
-protected:
-    explicit Camera(Rotator::Type rotatorType);
-
 protected:
     glm::mat4 m_projection, m_transform; // m_transform is view matrix
     float m_fov, m_aspectRatio, m_near, m_far;
-    float m_sensitivityPitch, m_sensitivityYaw, m_sensitivityRoll;
-    float m_speedForward, m_speedBack, m_speedRight, m_speedLeft;
 
 private:
     using Rotatable::setRotatorType;
--- src/std/camera/Camera.cpp
+++ src/std/camera/Camera.cpp
@@ -4,6 +4,26 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 namespace algine {
+Camera::Camera(Type type)
+    : m_projection(1.0f),
+      m_transform(1.0f),
+      m_fov(glm::radians(90.0f)),
+      m_aspectRatio(1.0f),
+      m_near(1.0f),
+      m_far(32.0f)
+{
+    switch (type) {
+        case Type::FPS: {
+            setRotatorType(Rotator::Type::Euler);
+            break;
+        }
+        case Type::Free: {
+            setRotatorType(Rotator::Type::Free);
+            break;
+        }
+    }
+}
+
 void Camera::translate() {
     m_translation = glm::translate(glm::mat4(1.0f), -m_pos);
 }
@@ -12,20 +32,26 @@ void Camera::updateMatrix() {
     m_transform = m_rotation * m_translation * m_scaling;
 }
 
-void Camera::goForward() {
-    m_pos += getForward() * m_speedForward;
+inline void goCamera(Camera *camera, const glm::vec3 &dPos) {
+    camera->changePos(dPos);
+    camera->translate();
+    camera->updateMatrix();
 }
 
-void Camera::goBack() {
-    m_pos += getBack() * m_speedBack;
+void Camera::goForward(float k) {
+    goCamera(this, getForward() * k);
 }
 
-void Camera::goRight() {
-    m_pos += getRight() * m_speedRight;
+void Camera::goBack(float k) {
+    goCamera(this, getBack() * k);
 }
 
-void Camera::goLeft() {
-    m_pos += getLeft() * m_speedLeft;
+void Camera::goRight(float k) {
+    goCamera(this, getRight() * k);
+}
+
+void Camera::goLeft(float k) {
+    goCamera(this, getLeft() * k);
 }
 
 void Camera::setFieldOfView(float fov) {
@@ -46,34 +72,6 @@ void Camera::setFar(float far) {
 
 void Camera::perspective() {
     m_projection = glm::perspective(m_fov, m_aspectRatio, m_near, m_far);
-}
-
-void Camera::setSensitivityPitch(float sensitivityPitch) {
-    m_sensitivityPitch = sensitivityPitch;
-}
-
-void Camera::setSensitivityYaw(float sensitivityYaw) {
-    m_sensitivityYaw = sensitivityYaw;
-}
-
-void Camera::setSensitivityRoll(float sensitivityRoll) {
-    m_sensitivityRoll = sensitivityRoll;
-}
-
-void Camera::setSpeedForward(float speedForward) {
-    m_speedForward = speedForward;
-}
-
-void Camera::setSpeedBack(float speedBack) {
-    m_speedBack = speedBack;
-}
-
-void Camera::setSpeedRight(float speedRight) {
-    m_speedRight = speedRight;
-}
-
-void Camera::setSpeedLeft(float speedLeft) {
-    m_speedLeft = speedLeft;
 }
 
 glm::mat4 Camera::getProjectionMatrix() const {
@@ -99,48 +97,4 @@ float Camera::getNear() const {
 float Camera::getFar() const {
     return m_far;
 }
-
-float Camera::getSensitivityPitch() const {
-    return m_sensitivityPitch;
-}
-
-float Camera::getSensitivityYaw() const {
-    return m_sensitivityYaw;
-}
-
-float Camera::getSensitivityRoll() const {
-    return m_sensitivityRoll;
-}
-
-float Camera::getSpeedForward() const {
-    return m_speedForward;
-}
-
-float Camera::getSpeedBack() const {
-    return m_speedBack;
-}
-
-float Camera::getSpeedRight() const {
-    return m_speedRight;
-}
-
-float Camera::getSpeedLeft() const {
-    return m_speedLeft;
-}
-
-Camera::Camera(Rotator::Type rotatorType)
-    : Rotatable(rotatorType),
-      m_projection(1.0f),
-      m_transform(1.0f),
-      m_fov(glm::radians(90.0f)),
-      m_aspectRatio(1.0f),
-      m_near(1.0f),
-      m_far(32.0f),
-      m_sensitivityPitch(0.0025f),
-      m_sensitivityYaw(0.0025f),
-      m_sensitivityRoll(0.0025f),
-      m_speedForward(1.0f),
-      m_speedBack(1.0f),
-      m_speedRight(1.0f),
-      m_speedLeft(1.0f) {}
 }
