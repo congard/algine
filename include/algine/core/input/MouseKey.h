fac7c3c3b38f517f1c066d0f598ab8ee46c70664
Daniel Bernar <dbcongard@gmail.com>
2021-01-07 17:15:02 +0200

    Add KeyboardKey.h and MouseKey.h - constants

---
+++ include/algine/core/input/MouseKey.h
@@ -0,0 +1,21 @@
+#ifndef ALGINE_MOUSEKEY_H
+#define ALGINE_MOUSEKEY_H
+
+namespace algine {
+enum class MouseKey {
+    Unknown,
+    Key1,
+    Key2,
+    Key3,
+    Key4,
+    Key5,
+    Key6,
+    Key7,
+    Key8,
+    Left = Key1,
+    Right = Key2,
+    Middle = Key3,
+};
+}
+
+#endif //ALGINE_MOUSEKEY_H
