0d74646d8d33b46ab979cc748a01ad252f914823
Daniel Bernar <dbcongard@gmail.com>
2021-01-23 13:48:31 +0200

    Update contrib.py script
    * Add current directory check
    * Download only one-header libs

--- contrib/contrib.py
+++ contrib/contrib.py
@@ -1,83 +1,35 @@
-# this file will install all contrib libs
-# must be launched directly from contrib directory!
-
-import shutil
-import urllib.request
 import os
-from zipfile import ZipFile
 
+if not os.getcwd().endswith("contrib"):
+    print("The script must be launched directly from the contrib directory")
+    exit(1)
 
-def index(x):
-    return int(x) - 1
-
+from urllib.request import urlretrieve
+from os.path import exists, join
 
 # src file, dst folder
 libs = [
-    ["https://github.com/g-truc/glm/releases/download/0.9.9.6/glm-0.9.9.6.zip", "glm"],
-    ["https://github.com/nlohmann/json/releases/download/v3.7.0/json.hpp", "nlohmann"],
+    ["https://github.com/nlohmann/json/releases/download/v3.9.1/json.hpp", "nlohmann"],
     ["https://raw.githubusercontent.com/nothings/stb/master/stb_image.h", "stb"],
-    ["https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h", "stb"],
-    ["https://github.com/assimp/assimp/archive/v.5.0.0.rc1.zip", "assimp"],
-    ["https://github.com/nigels-com/glew/releases/download/glew-2.1.0/glew-2.1.0.zip", "glew"]
+    ["https://raw.githubusercontent.com/nothings/stb/master/stb_image_write.h", "stb"]
 ]
 
-print("=== Contrib installer ===")
-print("One header libs:")
-print("1) glm\n"
-      "2) nlohmann/json.hpp\n"
-      "3) stb/stb_image.h\n"
-      "4) stb/std_image_write.h")
-print("Non one header libs:")
-print("5) assimp\n"
-      "6) glew")
-installLibs = input("What libs you want to install? Separate with spaces (default=all): ").split(" ")
+for lib in libs:
+    dstFolder = lib[1]
+    url = lib[0]
 
-# check the correctness of the entered data
-for lib in installLibs:
-    if index(lib) not in range(0, len(libs)):
-        print("Error: " + lib + " not in range [1; " + str(len(libs)) + "]")
-        exit(1)
-
-# [''] if pressed enter (all)
-if len(installLibs) == 1 and installLibs[0] == '':
-    installLibs = [l for l in range(1, len(libs) + 1)]
-
-print("Removing old libs (if exists)")
-for lib in installLibs:
-    dstFolder = libs[index(lib)][1]
-    if os.path.exists(dstFolder):
-        print("Removing " + dstFolder)
-        shutil.rmtree(dstFolder)
-
-# can't merge this loop with previous because of two "stb" dirs
-print("Creating new lib folders")
-for lib in installLibs:
-    dstFolder = libs[index(lib)][1]
-    if not os.path.exists(dstFolder):
-        print("Creating " + dstFolder)
+    if not exists(dstFolder):
         os.makedirs(dstFolder)
 
-for lib in installLibs:
-    dstFolder = libs[index(lib)][1]
-    url = libs[index(lib)][0]
-    filename = url.split("/")[-1]
+    filename = url.split("/")[-1]  # file name from url
+    filepath = join(dstFolder, filename)
 
-    print("Downloading " + url + " into " + dstFolder + " folder")
-    urllib.request.urlretrieve(url, os.getcwd() + "/" + dstFolder + "/" + filename)
+    if exists(filepath):
+        print("File " + filepath + " will be reinstalled")
 
-    if filename.endswith("zip"):
-        print("Extracting files from " + filename + " to " + dstFolder + "/__tmp__")
-        with ZipFile(dstFolder + "/" + filename, 'r') as zipObj:
-            extractedTo = dstFolder + "/__tmp__/" + zipObj.namelist()[0] + "/"
-            zipObj.extractall(dstFolder + "/__tmp__")
+        os.remove(filepath)
 
-        print("Moving files to " + dstFolder)
-        files = os.listdir(extractedTo)
-        for file in files:
-            os.rename(extractedTo + file, dstFolder + "/" + file)
+    print("Installing " + filename + " to " + filepath)
 
-        print("Clean up")
-        shutil.rmtree(dstFolder + "/__tmp__")
-        os.remove(dstFolder + "/" + filename)
-
-print("=== Done ===")
+    # download file
+    urlretrieve(url, filepath)
