b51203662c5503ee4d3e95a24f0143c070e50f51
Daniel Bernar <dbcongard@gmail.com>
2021-02-01 16:48:15 +0200

    Add gl.h - cross platform GL headers

---
+++ include/algine/gl.h
@@ -0,0 +1,17 @@
+#ifndef ALGINE_GL_H
+#define ALGINE_GL_H
+
+#ifdef __ANDROID__
+    // Include the latest possible header file( GL version header )
+    #if __ANDROID_API__ >= 24
+        #include <GLES3/gl32.h>
+    #elif __ANDROID_API__ >= 21
+        #include <GLES3/gl31.h>
+    #else
+        #include <GLES3/gl3.h>
+    #endif
+#else
+    #include <GL/glew.h>
+#endif
+
+#endif //ALGINE_GL_H
