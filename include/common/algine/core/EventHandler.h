769b2056908c0a3762997b6d2d83b86ddb92ed67
Daniel Bernar <dbcongard@gmail.com>
2021-02-09 20:04:24 +0200

    Add EventHandler
    Base class for classes like WindowEventHandler (desktop)
    and ScreenEventHandler (android)

---
+++ include/common/algine/core/EventHandler.h
@@ -0,0 +1,11 @@
+#ifndef ALGINE_EVENTHANDLER_H
+#define ALGINE_EVENTHANDLER_H
+
+namespace algine {
+class EventHandler {
+// Base class for classes like WindowEventHandler (desktop)
+// and ScreenEventHandler (android)
+};
+}
+
+#endif //ALGINE_EVENTHANDLER_H
