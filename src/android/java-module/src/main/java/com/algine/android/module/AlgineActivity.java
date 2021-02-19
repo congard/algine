37e9a51f3f5fbd63cdf435dbbb9113eeeb9d8b72
Daniel Bernar <dbcongard@gmail.com>
2021-02-19 13:46:42 +0200

    AndroidX instead of Android Support

--- src/android/java-module/src/main/java/com/algine/android/module/AlgineActivity.java
+++ src/android/java-module/src/main/java/com/algine/android/module/AlgineActivity.java
@@ -2,7 +2,7 @@ package com.algine.android.module;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 public class AlgineActivity extends Activity {
 
