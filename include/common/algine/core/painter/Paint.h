c69dc49a07ca84d75caa13a91db08cef2221623f
Daniel Bernar <dbcongard@gmail.com>
2021-07-07 16:59:33 +0300

    Add class Paint

---
+++ include/common/algine/core/painter/Paint.h
@@ -0,0 +1,40 @@
+#ifndef ALGINE_PAINT_H
+#define ALGINE_PAINT_H
+
+#include <algine/core/texture/Texture2DPtr.h>
+#include <algine/core/Color.h>
+
+#include <glm/mat4x4.hpp>
+
+namespace algine {
+class Paint {
+public:
+    enum class Source {
+        Color,
+        Texture
+    };
+
+public:
+    Paint();
+    explicit Paint(const Color &color);
+    explicit Paint(Texture2DPtr texture);
+
+    void setColor(const Color &color);
+    void setTexture(const Texture2DPtr &texture);
+    void setSource(Source source);
+    void setTransform(const glm::mat4 &transform);
+
+    const Color& getColor() const;
+    const Texture2DPtr& getTexture() const;
+    Source getSource() const;
+    const glm::mat4& getTransform() const;
+
+private:
+    Color m_color;
+    Texture2DPtr m_texture;
+    Source m_source;
+    glm::mat4 m_transform;
+};
+}
+
+#endif //ALGINE_PAINT_H
---
+++ src/common/core/painter/Paint.cpp
@@ -0,0 +1,57 @@
+#include <algine/core/painter/Paint.h>
+
+#include <utility>
+
+namespace algine {
+Paint::Paint()
+    : m_color(0xff000000),
+      m_texture(nullptr),
+      m_source(Source::Color),
+      m_transform(1.0f) {}
+
+Paint::Paint(const Color &color)
+    : m_color(color),
+      m_texture(nullptr),
+      m_source(Source::Color),
+      m_transform(1.0f) {}
+
+Paint::Paint(Texture2DPtr texture)
+    : m_color(0xff000000),
+      m_texture(std::move(texture)),
+      m_source(Source::Texture),
+      m_transform(1.0f) {}
+
+void Paint::setColor(const Color &color) {
+    m_source = Source::Color;
+    m_color = color;
+}
+
+void Paint::setTexture(const Texture2DPtr &texture) {
+    m_source = Source::Texture;
+    m_texture = texture;
+}
+
+void Paint::setSource(Source source) {
+    m_source = source;
+}
+
+void Paint::setTransform(const glm::mat4 &transform) {
+    m_transform = transform;
+}
+
+const Color& Paint::getColor() const {
+    return m_color;
+}
+
+const Texture2DPtr& Paint::getTexture() const {
+    return m_texture;
+}
+
+Paint::Source Paint::getSource() const {
+    return m_source;
+}
+
+const glm::mat4& Paint::getTransform() const {
+    return m_transform;
+}
+}
