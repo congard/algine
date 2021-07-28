bfeafe29e724c5bf1a0d320b9796cb51193c6fb3
Daniel Bernar <dbcongard@gmail.com>
2021-07-28 17:58:41 +0300

    Add enum Alignment

---
+++ include/common/algine/core/widgets/Alignment.h
@@ -0,0 +1,14 @@
+#ifndef ALGINE_ALIGNMENT_H
+#define ALGINE_ALIGNMENT_H
+
+namespace algine {
+enum Alignment {
+    Center  = 0b0000,
+    Left    = 0b0001,
+    Right   = 0b0010,
+    Top     = 0b0100,
+    Bottom  = 0b1000
+};
+}
+
+#endif //ALGINE_ALIGNMENT_H
