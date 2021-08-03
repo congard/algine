7ffc74045db1b8eddc3bae9480d972e65c1509b5
Daniel Bernar <dbcongard@gmail.com>
2021-08-03 17:09:02 +0300

    Add class ColorMap

---
+++ include/common/algine/std/ColorMap.h
@@ -0,0 +1,56 @@
+#ifndef ALGINE_COLORMAP_H
+#define ALGINE_COLORMAP_H
+
+#include <algine/core/texture/Texture2D.h>
+#include <algine/core/Color.h>
+
+#include <glm/vec2.hpp>
+
+#include <map>
+
+namespace algine {
+class ColorMap {
+private:
+    struct _ColorComparator {
+        bool operator()(const Color &lhs, const Color &rhs) const {
+            return lhs.value() < rhs.value();
+        }
+    };
+
+public:
+    using Colors = std::map<Color, glm::ivec2, _ColorComparator>;
+
+public:
+    ColorMap();
+    ColorMap(uint width, uint height, uint format = Texture::RGBA8UI);
+
+    void create(uint width, uint height, uint format = Texture::RGBA8UI);
+    void update();
+
+    void setColors(const Colors &colors);
+    void setColor(const Color &color, const glm::ivec2 &uv);
+
+    const Colors& getColors() const;
+    const glm::ivec2& getUV(const Color &color) const;
+    const Color& getColor(const glm::ivec2 &uv) const;
+
+    void setWidth(uint width);
+    void setHeight(uint height);
+    void setFormat(uint format);
+
+    uint getWidth() const;
+    uint getHeight() const;
+    uint getFormat() const;
+
+    bool exists(const Color &color) const;
+    bool exists(const glm::ivec2 &uv) const;
+
+    const Texture2DPtr& get() const;
+
+private:
+    Texture2DPtr m_texture;
+    Colors m_colors;
+};
+}
+
+#endif //ALGINE_COLORMAP_H
---
+++ src/common/std/ColorMap.cpp
@@ -0,0 +1,178 @@
+#include <algine/std/ColorMap.h>
+#include <algine/core/PtrMaker.h>
+
+#include <glm/gtx/string_cast.hpp>
+
+#include <tulz/Array.h>
+
+#include <stdexcept>
+
+namespace algine {
+ColorMap::ColorMap() = default;
+
+ColorMap::ColorMap(uint width, uint height, uint format) {
+    create(width, height, format);
+}
+
+void ColorMap::create(uint width, uint height, uint format) {
+    m_texture = PtrMaker::make();
+    m_texture->bind();
+    m_texture->setDimensions(width, height);
+    m_texture->setFormat(format);
+    m_texture->setParams(std::map<uint, uint> {
+        {Texture::MinFilter, Texture::Nearest},
+        {Texture::MagFilter, Texture::Nearest},
+        {Texture::WrapU, Texture::ClampToEdge},
+        {Texture::WrapV, Texture::ClampToEdge}
+    });
+}
+
+inline auto componentsCount(uint format) {
+    switch (format) {
+        case Texture::Red:
+        case Texture::RedInteger:
+            return 1;
+        case Texture::RG:
+        case Texture::RGInteger:
+            return 2;
+        case Texture::RGB:
+        case Texture::RGBInteger:
+            return 3;
+        case Texture::RGBA:
+        case Texture::RGBAInteger:
+            return 4;
+        default: throw std::invalid_argument("Invalid format");
+    }
+}
+
+template<typename T>
+inline void fillData(T *data, const ColorMap::Colors &colors, uint components, uint width) {
+    switch (components) {
+        case 1:
+            for (const auto &p : colors) {
+                auto &color = p.first;
+                auto &uv = p.second;
+                data[uv.y * width + uv.x] = color.red<T>();
+            }
+            break;
+        case 2:
+            for (const auto &p : colors) {
+                auto &color = p.first;
+                auto &uv = p.second;
+                auto index = (uv.y * width + uv.x) * 2;
+                data[index + 0] = color.red<T>();
+                data[index + 1] = color.green<T>();
+            }
+            break;
+        case 3:
+            for (const auto &p : colors) {
+                auto &color = p.first;
+                auto &uv = p.second;
+                auto index = (uv.y * width + uv.x) * 3;
+                data[index + 0] = color.red<T>();
+                data[index + 1] = color.green<T>();
+                data[index + 2] = color.blue<T>();
+            }
+
+            break;
+        case 4:
+            for (const auto &p : colors) {
+                auto &color = p.first;
+                auto &uv = p.second;
+                auto index = (uv.y * width + uv.x) * 4;
+                data[index + 0] = color.red<T>();
+                data[index + 1] = color.green<T>();
+                data[index + 2] = color.blue<T>();
+                data[index + 3] = color.alpha<T>();
+            }
+            break;
+        default: break;
+    }
+}
+
+void ColorMap::update() {
+    auto width = getWidth();
+    auto height = getHeight();
+
+    auto formatInfo = Texture::getFormatInfo(m_texture->getFormat());
+    auto components = componentsCount(formatInfo.baseFormat);
+
+    void *data = calloc(components * width * height, 4);
+
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // TODO: to Engine
+
+    m_texture->bind();
+
+    if (formatInfo.dataType == DataType::Float) {
+        fillData((float*) data, m_colors, components, width);
+        m_texture->update(formatInfo.baseFormat, DataType::Float, data);
+    } else {
+        fillData((int*) data, m_colors, components, width);
+        m_texture->update(formatInfo.baseFormat, DataType::Int, data);
+    }
+
+    free(data);
+}
+
+void ColorMap::setColors(const Colors &colors) {
+    m_colors = colors;
+}
+
+void ColorMap::setColor(const Color &color, const glm::ivec2 &uv) {
+    m_colors[color] = uv;
+}
+
+const ColorMap::Colors& ColorMap::getColors() const {
+    return m_colors;
+}
+
+const glm::ivec2& ColorMap::getUV(const Color &color) const {
+    return m_colors.at(color);
+}
+
+const Color& ColorMap::getColor(const glm::ivec2 &uv) const {
+    for (const auto &p : m_colors) {
+        if (p.second == uv) {
+            return p.first;
+        }
+    }
+
+    throw std::runtime_error("Color at " + glm::to_string(uv) + " not found");
+}
+
+void ColorMap::setWidth(uint width) {
+    m_texture->setWidth(width);
+}
+
+void ColorMap::setHeight(uint height) {
+    m_texture->setHeight(height);
+}
+
+void ColorMap::setFormat(uint format) {
+    m_texture->setFormat(format);
+}
+
+uint ColorMap::getWidth() const {
+    return m_texture->getWidth();
+}
+
+uint ColorMap::getHeight() const {
+    return m_texture->getHeight();
+}
+
+uint ColorMap::getFormat() const {
+    return m_texture->getFormat();
+}
+
+bool ColorMap::exists(const Color &color) const {
+    return m_colors.find(color) != m_colors.end();
+}
+
+bool ColorMap::exists(const glm::ivec2 &uv) const {
+    return std::any_of(m_colors.begin(), m_colors.end(), [&](const auto &p) { return p.second == uv; });
+}
+
+const Texture2DPtr& ColorMap::get() const {
+    return m_texture;
+}
+}
