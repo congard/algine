8039be0bdef500fe0a65d61bbd60f56c265b1e40
Daniel Bernar <dbcongard@gmail.com>
2021-08-09 13:46:47 +0300

    Fix blur shader for OpenGLES

--- src/common/core/glsl-shaders/Blur.fs.glsl
+++ src/common/core/glsl-shaders/Blur.fs.glsl
@@ -6,19 +6,20 @@ uniform float kernel[KERNEL_RADIUS];
 layout (location = 0) out vecout fragColor;
 
 void main() {
-    vec2 texOffset = 1.0f / textureSize(image, 0); // gets size of single texel
+    vec2 texelSize = 1.0f / vec2(textureSize(image, 0)); // gets size of single texel
 
     fragColor = texture(image, texCoord).texComponent * kernel[0]; // current fragment’s contribution
 
     #ifdef ALGINE_HORIZONTAL
-        #define texCoordP texCoord + vec2(texOffset.x * i, 0.0f)
-        #define texCoordM texCoord - vec2(texOffset.x * i, 0.0f)
+        #define texCoordP texCoord + vec2(texelSize.x * iteration, 0.0f)
+        #define texCoordM texCoord - vec2(texelSize.x * iteration, 0.0f)
     #else
-        #define texCoordP texCoord + vec2(0.0f, texOffset.y * i)
-        #define texCoordM texCoord - vec2(0.0f, texOffset.y * i)
+        #define texCoordP texCoord + vec2(0.0f, texelSize.y * iteration)
+        #define texCoordM texCoord - vec2(0.0f, texelSize.y * iteration)
     #endif
 
     for (int i = 1; i < KERNEL_RADIUS; i++) {
+        const float iteration = float(i);
         fragColor += kernel[i] * (texture(image, texCoordP).texComponent + texture(image, texCoordM).texComponent);
     }
 }
