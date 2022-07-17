0d403f5cb656245c8697473baa1800abe1184202
Daniel <dbcongard@gmail.com>
2022-07-17 20:05:44 +0200

    Silence clang warning

--- src/common/core/djb2.h
+++ src/common/core/djb2.h
@@ -7,7 +7,7 @@ inline unsigned long hash_djb2(unsigned char *str) {
     unsigned long hash = 5381;
     int c;
 
-    while (c = *str++) {
+    while ((c = *str++)) {
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
     }
 
