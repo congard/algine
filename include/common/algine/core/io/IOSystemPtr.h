77dc1e2a116cf2bf2da79ed2bc74284d68e6abc4
Daniel <dbcongard@gmail.com>
2023-08-05 15:39:19 +0200

    Add IOSystemPtr: alias to Ptr<IOSystem>
    Added to improve code readability

---
+++ include/common/algine/core/io/IOSystemPtr.h
@@ -0,0 +1,11 @@
+#ifndef ALGINE_TESTS_IOSYSTEMPTR_H
+#define ALGINE_TESTS_IOSYSTEMPTR_H
+
+#include <algine/core/Ptr.h>
+
+namespace algine {
+class IOSystem;
+using IOSystemPtr = Ptr<IOSystem>;
+}
+
+#endif //ALGINE_TESTS_IOSYSTEMPTR_H
