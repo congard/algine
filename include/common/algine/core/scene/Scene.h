806f76d3e4c08d59272aa051b1c01c8d0a051ad4
Daniel <dbcongard@gmail.com>
2023-09-30 00:24:04 +0200

    Scene: add functions for showing and hiding
    This commit adds the following functions:
    
    - public
        - `show`
        - `hide`
        - `isShown`
        - `isHidden`
    - protected
        - `onShow`
        - `onHide`
        - `onRender`
    
    For more info, check the changes.

--- include/common/algine/core/scene/Scene.h
+++ include/common/algine/core/scene/Scene.h
@@ -8,11 +8,41 @@ class AL_EXPORT Scene: public Object {
 public:
     explicit Scene(Object *parent = nullptr);
 
-    virtual void render();
+    /**
+     * Shows the current scene, if it is hidden.
+     * @note `onShow` will be called only if the
+     * current scene is hidden.
+     */
+    void show();
+
+    /**
+     * Hides the current scene, if it is shown.
+     * @note `onHide` will be called only if the
+     * current scene is shown.
+     */
+    void hide();
+
+    /**
+     * Renders the current scene if it is shown.
+     * @note `onRender` will be called only if the
+     * current scene is shown.
+     */
+    void render();
+
+    bool isShown() const;
+    bool isHidden() const;
 
     using Object::addChild;
     using Object::removeChild;
     using Object::removeChildren;
+
+protected:
+    virtual void onShow();
+    virtual void onHide();
+    virtual void onRender();
+
+private:
+    bool m_isShown;
 };
 } // algine
 
--- src/common/core/scene/Scene.cpp
+++ src/common/core/scene/Scene.cpp
@@ -2,7 +2,38 @@
 
 namespace algine {
 Scene::Scene(Object *parent)
-    : Object(parent) {}
+    : Object(parent),
+      m_isShown(false) {}
 
-void Scene::render() {}
+void Scene::show() {
+    if (isHidden()) {
+        onShow();
+        m_isShown = true;
+    }
+}
+
+void Scene::hide() {
+    if (isShown()) {
+        onHide();
+        m_isShown = false;
+    }
+}
+
+void Scene::render() {
+    if (isShown()) {
+        onRender();
+    }
+}
+
+bool Scene::isShown() const {
+    return m_isShown;
+}
+
+bool Scene::isHidden() const {
+    return !m_isShown;
+}
+
+void Scene::onShow() {}
+void Scene::onHide() {}
+void Scene::onRender() {}
 } // algine
